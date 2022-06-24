f2eefd0336eb4356e6c4ffa77afac8fd08e92667
Jackson Ludwig <42984254+jacksonludwig@users.noreply.github.com>
2022-06-23 23:02:12 -0400

    highlight unpacked, method, class field

--- after/queries/typescript/highlights.scm
+++ after/queries/typescript/highlights.scm
@@ -14,3 +14,14 @@
 ;; Highlight variable declarations as definition
 (variable_declarator
   name: (identifier) @alabaster.definition)
+
+;; Highlight unpacked declarations as definition
+(shorthand_property_identifier_pattern) @alabaster.definition
+
+;; Highlight method names as definition
+(method_definition
+  name: (property_identifier) @alabaster.definition)
+
+;; Highlight class field definitions as definition
+(public_field_definition
+  name: (property_identifier) @alabaster.definition)
